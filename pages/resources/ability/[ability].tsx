4d1fce34ea2ca5abeacdd1c7c1e7ab52fc318ca1
judsonjames <judsonjames96@gmail.com>
2021-04-11 14:33:26 -0400

    Testing SSG for Ability to check if Netlify can host correctly

--- pages/resources/ability/[ability].tsx
+++ pages/resources/ability/[ability].tsx
@@ -1,32 +1,62 @@
 import { Text } from "@chakra-ui/react";
-import { GetServerSideProps } from "next";
+import axios, { AxiosResponse } from "axios";
 import React from "react";
-import { QueryClient, useQuery } from "react-query";
-import { dehydrate } from "react-query/hydration";
 import { getAbility } from "../../../apis/pokeapi";
+import AbilityType from "../../../apis/pokeapi/types/ability";
 import SiteLayout from "../../../components/SiteLayout";
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const ability: string = context.params.ability as string;
-  const queryClient: QueryClient = new QueryClient();
-  await queryClient.prefetchQuery("ability", () => getAbility(ability));
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const ability: string = context.params.ability as string;
+//   const queryClient: QueryClient = new QueryClient();
+//   await queryClient.prefetchQuery("ability", () => getAbility(ability));
+
+//   return {
+//     props: {
+//       ability,
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//   };
+// };
+
+export async function getStaticPaths() {
+  const response: AxiosResponse = await axios.get(
+    "https://pokeapi.co/api/v2/ability/?limit=1000"
+  );
+  const results: any[] = response.data.results;
+
+  const paths = results.map((result) => ({
+    params: { ability: result.name },
+  }));
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const name: string = params.ability;
+  const ability: AbilityType = await getAbility(name);
+  // const ability = params.ability;
+  // const queryClient: QueryClient = new QueryClient();
+  // await queryClient.prefetchQuery("ability", () => getAbility(ability));
 
   return {
     props: {
       ability,
-      dehydratedState: dehydrate(queryClient),
+      // dehydratedState: dehydrate(queryClient),
     },
   };
-};
+}
 
 export default function Ability(props) {
   const { ability } = props;
-  const { data } = useQuery("ability", () => getAbility(ability));
+  // const { data } = useQuery("ability", () => getAbility(ability));
   return (
     <SiteLayout>
-      <Text>Ability :: {data.name}</Text>
+      <Text>Ability :: {ability.name}</Text>
       <br />
-      <Text>{data.effect_entries[1].effect}</Text>
+      {/* <Text>{ability.effect_entries[1].effect}</Text> */}
     </SiteLayout>
   );
 }
